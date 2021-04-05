da3650d4a345593516f7c03268b28705f400c1ee
Kevin Silvestre <kevsilvo@gmail.com>
2021-04-05 15:08:50 -0400

    API call success

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [nasa, setNasa] = useState({});
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(res => {
+        console.log(res.data);
+        setNasa(res.data);
+
+      })
+      .catch(err => {
+
+      })
+
+  }, [])
+
+
   return (
     <div className="App">
+      <img src={nasa.url} />
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        {nasa.explanation}
       </p>
     </div>
   );
